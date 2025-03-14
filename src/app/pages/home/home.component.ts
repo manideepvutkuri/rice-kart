c735119b102411a0581af2998938383f97cd7869
mvutkuri <technofreak0484@gmail.com>
2025-03-14 09:44:40 +0530

    changes

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -27,6 +27,8 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Router, RouterModule } from '@angular/router';
 import { CartService } from '../../services/cart.service';
+import { AdminRiceService } from '../../services/admin-rice.service';
+import { RiceVariety } from '../../models/rice-variety.model';
 
 @Component({
   selector: 'app-home',
@@ -36,15 +38,23 @@ import { CartService } from '../../services/cart.service';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
-  products = [
-    { id: 1, name: 'Basmati Rice', price: 120, image: 'assets/images/basmati.jpg' },
-    { id: 2, name: 'Brown Rice', price: 90, image: 'assets/images/sri.webp' },
-    { id: 3, name: 'Jasmine Rice', price: 150, image: 'assets/images/ind.webp' }
-  ];
+  riceVarieties: RiceVariety[] = [];
+  // products = [
+  //   { id: 1, name: 'Basmati Rice', price: 120, image: 'assets/images/basmati.jpg' },
+  //   { id: 2, name: 'Brown Rice', price: 90, image: 'assets/images/sri.webp' },
+  //   { id: 3, name: 'Jasmine Rice', price: 150, image: 'assets/images/ind.webp' }
+  // ];
 
-  constructor(private router: Router,public cartService: CartService) {}
-
-  viewProduct(product: any) {
-    this.router.navigate(['/product', product.id], { state: { product } });
+  constructor(private router: Router,public cartService: CartService,private adminRiceService: AdminRiceService) {}
+  ngOnInit(): void {
+    this.adminRiceService.getAllRiceVarieties().subscribe((rice) => {
+      this.riceVarieties = rice;
+    });
+  }
+  // viewProduct(product: any) {
+  //   this.router.navigate(['/product', product.id], { state: { product } });
+  // }
+  viewDetails(rice: RiceVariety) {
+    this.router.navigate(['/product', rice.id], { state: { product: rice } });
   }
 }
